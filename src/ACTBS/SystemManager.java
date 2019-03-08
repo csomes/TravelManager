cd83ac23a1c283f6ec90f4956b5618e1b54cae92
Colton Somes <coltonsomes@gmail.com>
2019-03-07 17:17:36 -0800

    changes

--- src/ACTBS/SystemManager.java
+++ src/ACTBS/SystemManager.java
@@ -8,33 +8,42 @@ public class SystemManager {
 	
     public void createAirport(String name) {
     	//How should we handle this if we don't use a Try-Catch? is this implementation OK?
-    	Airport airport = new Airport(name);
-    	
-    	for(Airport i: airports) {
-    		if(airport.getName().equals(i.getName())){
-    			System.out.println("Airport creation failed");
-    			throw new RuntimeException("Duplicate Airport name"); 
-    		}
+    	try{
+    		Airport airport = new Airport(name);
+        	
+        	for(Airport i: airports) {
+        		if(airport.getName().equals(i.getName())){
+        			System.out.println("Airport creation failed");
+        			throw new RuntimeException("Duplicate Airport name"); 
+        		}
+        	}
+        	
+        	airports.add(airport); 
+        	System.out.println("Airport " + airport.getName() + " created successfully"); 
+    	}catch(RuntimeException R) {
+    		System.out.println(R);
     	}
     	
-    	airports.add(airport); 
-    	System.out.println("Airport " + airport.getName() + " created successfully"); 
+    	
     	
     }
     
     public void createAirline(String name) {
+    	try {
+    		Airline airline = new Airline(name);
     	
-    	Airline airline = new Airline(name);
-    	
-    	for(Airline i: airlines) {
-    		if(airline.getName().equals(i.getName())){
-    			System.out.println("Airline creation failed");
-    			throw new RuntimeException("Duplicate Airline name"); 
+    		for(Airline i: airlines) {
+    			if(airline.getName().equals(i.getName())){
+    				System.out.println("Airline creation failed");
+    				throw new RuntimeException("Duplicate Airline name"); 
+    			}
     		}
-    	}
     	
-    	airlines.add(airline); 
-    	System.out.println("Airline " + airline.getName() + " created successfully"); 
+    		airlines.add(airline); 
+    		System.out.println("Airline " + airline.getName() + " created successfully"); 
+    	}catch(RuntimeException R) {
+    		System.out.println(R);
+    	}
     	
     }
     public void createFlight(String name1, String name2, String name3, int num1, int num2, int num3, String name4) {}
