3f1d063a31f06fd7b6934f10bbe3a9a42a93d1e6
Colton Somes <coltonsomes@gmail.com>
2019-03-07 17:21:57 -0800

    Revert "Created some files and stubbed out methods"
    This reverts commit d9eafdb71e7e4b019f0e8393c50bc40e3acd0a9b.

--- src/ACTBS/Section.java
+++ src/ACTBS/Section.java
@@ -1,20 +1,4 @@
 package ACTBS;
 
 public class Section {
-	private SeatClass _seatClass;
-	private int[][] _seats; 
-	
-	public Section(SeatClass seatClass) {
-		
-		this._seatClass = _seatClass;
-		
-	}
-	
-	public SeatClass getSeatClass() {
-		return this._seatClass; 
-	}
-	
-	public boolean bookSeat(int row, int col) {
-		return false; 
-	}
 }
--- src/ACTBS/SystemManager.java
+++ src/ACTBS/SystemManager.java
@@ -1,25 +1,9 @@
 package ACTBS;
 
-import java.util.*;
-
 public class SystemManager {
-	public List<Airport> airports = new ArrayList<Airport>(); 
-	
-    public void createAirport(String name) throws Exception {
-    	Airport airport = new Airport(name);
-    	for(Airport i: airports) {
-    		if(airport.getName().equals(i.getName())){
-    			throw new Exception("Duplicate Airport name"); 
-    		} else {
-    			airports.add(airport); 
-    		}
-    	}
-    	
-    }
-    
-    public void createAirline(String name) {
-    	
-    }
+
+    public void createAirport(String name) {}
+    public void createAirline(String name) {}
     public void createFlight(String name1, String name2, String name3, int num1, int num2, int num3, String name4) {}
     public void createSection(String name1, String name2, int num2, int num3, SeatClass name4) {}
     public void findAvailableFlights(String num1, String num2) {}
